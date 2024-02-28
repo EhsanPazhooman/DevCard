b33f1050d4d287406393052a52689aec18ea16c3
Ehsan Pazhooman <ehspazh@gmail.com>
2024-02-29 02:04:54 +0330

    project section from index page moved to partial view projects

---
+++ DevCard_MVC/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿namespace DevCard_MVC.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+    }
+
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+}
