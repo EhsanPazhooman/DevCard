7f4391590a4145b7c6990478e3100fd9599b508d
Ehsan Pazhooman <ehspazh@gmail.com>
2024-02-29 19:12:35 +0330

    Build the view component of projects

--- DevCard_MVC/Models/Project.cs
+++ DevCard_MVC/Models/Project.cs
@@ -5,7 +5,17 @@
         public long Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
         public string Client { get; set; }
+
+        public Project(long id, string title, string description,string image, string client)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+            Client = client;
+        }
     }
 
     public class Article
---
+++ DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>
+            {
+                new Project(1,"تاکسی","درخواست آنلاین تاکسی برای سفرهای درون شهری","project-1.jpg","Snapp"),
+                new Project(2, "زودفود", "درخواست آنلاین غذا برای سراسر کشور","project-figure-2.jpg","Snapfood"),
+                new Project(3,"مدارس","سیستم مدیریت یکپارچه مدارس","project-figure-1.jpg","monop"),
+                new Project(4,"فضاپیما","برنامه مدیریت فضاپیماهای ناسا","project-6.jpg","Nasa"),
+            };
+            return View("_Projects",projects);
+        }
+    }
+}
